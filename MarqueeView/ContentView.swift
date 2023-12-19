a97b5251d816e2a3c7f2459d239183466992f51f
Monty Harper <132530279+MontyHarper@users.noreply.github.com>
2023-12-19 13:18:58 -0600

    Initial Commit

---
+++ MarqueeView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MarqueeView
+//
+//  Created by Monty Harper on 12/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarqueeView/MarqueeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarqueeViewApp.swift
+//  MarqueeView
+//
+//  Created by Monty Harper on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct MarqueeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
